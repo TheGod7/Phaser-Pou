56d17ac969649f1f12caecaf308ffa1105f8bf8c
TheGod7 <br.iancefi12@gmail.com>
2025-08-13 17:16:13 -0600

    test

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,5 +1,26 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
 function App() {
-    return <div className="w-full h-screen bg-black/90 text-white"></div>;
+    return (
+        <div className="w-full h-screen bg-black/90 text-white lg:p-40  sm:p-20 p-10 select-none">
+            <h1 className="text-6xl font-bold">Phaser Game</h1>
+
+            <section className="mt-10 bg-gray-950/30 rounded-3xl p-10">
+                {/* <h2 className="text-4xl font-bold"></h2>
+                 */}
+
+                <p>idandksionjaksdb</p>
+            </section>
+            {/* <SyntaxHighlighter
+                language="javascript"
+                style={atomOneDark}
+                showLineNumbers={true}
+            >
+                {"const a = 1;"}
+            </SyntaxHighlighter> */}
+        </div>
+    );
 }
 
 export default App;
